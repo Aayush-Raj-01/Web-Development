aa5a85273323546cd28f3238b7b0fc873c70c65d
Aayush Raj <aush110804@gmail.com>
2025-12-03 23:45:09 +0530

    learning

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,14 @@
-console.log("Aayush")
+const accountid = 1444553
+let accountEmail = "Ayush@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+
+accountEmail = "hac@gamil.com"
+accountPassword = "212121"
+accountCity = "MUbai"
+
+
+console.log(accountid);
+
+console.table([accountid,accountEmail,accountPassword,accountCity])
