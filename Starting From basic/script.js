58d728c3ee2170a33f57bb969f979632d7750f67
Aayush Raj <aush110804@gmail.com>
2026-01-14 01:06:25 +0530

    Web Dev

---
+++ Starting From basic/script.js
@@ -0,0 +1,7 @@
+const nt = document.querySelector('.navbar-toggle');
+const nm = document.querySelector('.navbar-menu');
+
+nt.addEventListener('click' , () => {
+    nt.classList.toggle('active');
+    nm.classList.toggle('active');
+});
--- javascript/app2.js
+++
@@ -1,4 +0,0 @@
-"use strict"; // treat all js code as newer version
-
-// Learned different types of string converstion
-fgh
